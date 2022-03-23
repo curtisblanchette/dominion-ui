173ff0bf7e01b2a98e87484ea43bd49e7975430f
curtisblanchette <hello@curtisblanchette.com>
2022-03-23 08:39:49 -0700

    feat(flow): add breadcrumb historical navigation

--- src/app/flow/flow.component.ts
+++ src/app/flow/flow.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnDestroy, OnInit, ViewChild } from "@angular/core";
 import { FlowService } from "./flow.service";
-import { FlowDirective, FlowRouter, FlowStepItem } from "./common";
+import { FlowDirective, FlowRouter, FlowStep } from "./common";
 import { IStep } from "./common/flow.step.interface";
 
 
@@ -9,15 +9,16 @@ import { IStep } from "./common/flow.step.interface";
     <div>
       <h3>Call Flow</h3>
       <ng-template flowHost></ng-template>
-      <button (click)="onBack()">Back</button>
-      <button (click)="onNext()">Next</button>
+      <button (click)="onBack()" [disabled]="breadcrumbs.length === 1">Back</button>
+      <button (click)="onNext()" >Next</button>
     </div>
   `
 })
 export class FlowComponent implements OnInit, OnDestroy {
 
-  currentStep: FlowStepItem;
-  step: FlowStepItem;
+  currentStep: FlowStep;
+  step: FlowStep;
+  breadcrumbs: FlowStep[] = [];
   @ViewChild(FlowComponent) flow: FlowComponent;
   @ViewChild(FlowDirective, {static: true}) flowHost!: FlowDirective;
 
@@ -31,42 +32,50 @@ export class FlowComponent implements OnInit, OnDestroy {
     this.step = this.flowService.getFirstStep();
     this.currentStep = this.step;
     this.renderComponent(this.currentStep);
+    this.breadcrumbs.push(this.currentStep);
   }
 
   public onNext() {
     // find a link where the "from" is equal to "currentStep"
     const link = this.flowService.links.find(link => link.from.id === this.currentStep.id);
 
-    if (link?.to instanceof FlowStepItem) {
+    if (link?.to instanceof FlowStep) {
       const step = this.flowService.steps.find(step => step.id === link.to.id);
 
       if (step) {
+        this.breadcrumbs.push(step);
         this.renderComponent(step);
       }
     } else if (link?.to instanceof FlowRouter) {
       const step = link.to.evaluate();
+      this.breadcrumbs.push(step);
       this.renderComponent(step);
     }
 
   }
 
   public onBack() {
-    // find a link where the "from" is equal to "currentStep"
-    const link = this.flowService.links.find(link => link.to.id === this.currentStep.id);
-
-    if (link) {
-      const step = this.flowService.steps.find(step => step.id === link.from.id);
-
-      if (step) {
-        this.renderComponent(step);
-      }
-
-    } else {
-      // Houston, we have a problem.
+    if(this.breadcrumbs.length > 1) {
+      this.breadcrumbs.pop();
+      this.renderComponent(this.breadcrumbs[this.breadcrumbs.length - 1]);
     }
+
+    // find a link where the "from" is equal to "currentStep"
+    // const link = this.flowService.links.find(link => link.to.id === this.currentStep.id);
+    //
+    // if (link?.from instanceof FlowStep) {
+    //   const step = this.flowService.steps.find(step => step.id === link.from.id);
+    //
+    //   if (step) {
+    //     this.renderComponent(step);
+    //   }
+    //
+    // } else if (link?.from) {
+    //   // Houston, we have a problem.
+    // }
   }
 
-  public renderComponent(step: FlowStepItem) {
+  public renderComponent(step: FlowStep) {
     this.currentStep = step;
     this.flowHost.viewContainerRef.clear();
 
