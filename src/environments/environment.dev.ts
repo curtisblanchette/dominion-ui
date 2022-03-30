2d4a671ce130df3d9817c7563ee3c319e1cf20d6
curtisblanchette <hello@curtisblanchette.com>
2022-03-30 13:58:36 -0700

    feat(develop): update cognito credentials

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -3,6 +3,6 @@ export const environment = {
 
   dominion_api_url: 'https://dominion-alb-dev.4iiz.io/api/v1/',
   support_url: 'https://desk.zoho.com/portal/4iizportal/home',
-  cognito_userPoolId : 'us-east-1_51XHTmNPf',
-  cognito_clientId : '7nff0348f3s8pm3d9qme79mqe2'
+  cognito_userPoolId : 'us-east-1_uhZfUvrtc',
+  cognito_clientId : '785kcsihnklge68g1k3blddh81'
 };
