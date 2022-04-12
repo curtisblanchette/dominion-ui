1235616607c47a34978fa935f493c3d8405b4ade
curtisblanchette <hello@curtisblanchette.com>
2022-04-12 14:10:25 -0700

    fix(develop): add acting_for demo db in environment.dev.ts

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -4,5 +4,6 @@ export const environment = {
   dominion_api_url: 'https://dominion-alb-dev.4iiz.io/api/v1/',
   support_url: 'https://desk.zoho.com/portal/4iizportal/home',
   cognito_userPoolId : 'us-east-1_vdnTDARJY',
-  cognito_clientId : '39o4fsufibd00thi5c2a60o2bg'
+  cognito_clientId : '39o4fsufibd00thi5c2a60o2bg',
+  acting_for: '3019a3cf-1d99-4837-b21a-8d418d08041b'
 };
