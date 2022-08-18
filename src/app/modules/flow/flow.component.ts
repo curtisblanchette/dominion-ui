d2230ffac9b3b3e31911ea48f24c63913cf30f3e
Raj kumar patel <rajkumar@systango.com>
2022-08-18 17:59:35 +0530

    fix(II-2767):css fix

--- src/app/modules/flow/flow.component.ts
+++ src/app/modules/flow/flow.component.ts
@@ -12,7 +12,6 @@ import { Dialog } from '@angular/cdk/dialog';
 import { UntilDestroy } from '@ngneat/until-destroy';
 
 import { DropdownItem } from '../../common/components/interfaces/dropdownitem.interface';
-import { ICallNote } from '@4iiz/corev2';
 
 @UntilDestroy()
 @Component({
@@ -30,21 +29,6 @@ export class FlowComponent implements AfterContentInit, OnDestroy {
   isLastStep$: Observable<boolean>;
   status$: Observable<string>;
 
-  public tinymceOptions = {
-    branding: false,
-    menubar: false,
-    toolbar: 'bold italic strikethrough underline align',
-    statusbar: false,
-    content_style: `
-      body {
-        font-family: Roboto, Arial, sans-serif;
-        font-size: 12px;
-        font-weight: 500;
-        line-height: 1.5em;
-        color: #C6CEED;
-      }`
-  };
-
   public menuItems: IDropDownMenuItem[] = [
     {
       label: 'Object',
@@ -61,7 +45,6 @@ export class FlowComponent implements AfterContentInit, OnDestroy {
   public objections$: Observable<DropdownItem>;
 
   @ViewChild(FlowHostDirective, {static: true}) flowHost!: FlowHostDirective;
-  @ViewChild('tinymce') tinymce: any;
 
   constructor(
     private store: Store<fromFlow.FlowState>,
@@ -135,10 +118,6 @@ export class FlowComponent implements AfterContentInit, OnDestroy {
   }
 
   public ngOnDestroy() {
-  }
-
-  public saveNotes(html: any): Observable<ICallNote> {
-    return this.flowService.updateNote(html);
   }
 
   public openNotesDialog() {
