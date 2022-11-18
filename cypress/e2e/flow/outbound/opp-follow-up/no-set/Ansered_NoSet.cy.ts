e814c42c733a91dec1588d7856b677f2a36ed479
Curt Blanchette <6607883+curtisblanchette@users.noreply.github.com>
2022-11-18 09:02:53 -0800

    fix(cypress): these can only be passed when recording

--- cypress/e2e/flow/outbound/opp-follow-up/no-set/Ansered_NoSet.cy.ts
+++ cypress/e2e/flow/outbound/opp-follow-up/no-set/Ansered_NoSet.cy.ts
@@ -4,44 +4,43 @@ describe('Outbound Call Flow - Answered - No Set', () => {
 
     // Select Outbound and proceed
     cy.get('[data-qa="call_direction"]').within(() => {
-      cy.get('label').contains('Outbound').click()
-    })
-    cy.get('[data-qa="next"]').click()
+      cy.get('label').contains('Outbound').click();
+    });
+    cy.get('[data-qa="next"]').click();
 
     // Select Opp Follow up
     cy.get('fiiz-radio').within(() => {
       cy.get('label').contains('Opportunity Follow Up').click()
-    })
-    cy.get('[data-qa="next"]').click()
+    });
+    cy.get('[data-qa="next"]').click();
 
     // Select Deal with No Set from List
     cy.get('[data-qa="table-row"]').within((row) => {
       cy.wrap(row).each(($el, $index, $list) => {
         if ($el.children().eq(3).text() == 'No Set') {
-          cy.wrap($el.children().eq(3)).click()
+          cy.wrap($el.children().eq(3)).click();
           return false;
         }
-      })
-    })
-    cy.get('[data-qa="next"]').click()
+      });
+    });
 
     // Opp Follow up
     cy.get('[data-qa="call-status"]').within(($form) => {
-      cy.get('fiiz-dropdown').first().click()
+      cy.get('fiiz-dropdown').first().click();
       cy.get('[data-qa="dropdown-items"] button').contains('Answered').click()
 
-      cy.get('fiiz-dropdown').eq(1).click()
+      cy.get('fiiz-dropdown').eq(1).click();
       cy.get('[data-qa="dropdown-items"] button').contains('No Set').click()
-    })
-    cy.get('[data-qa="next"]').click()
+    });
+    cy.get('[data-qa="next"]').click();
 
     // Takes Notes
-    cy.get('iframe').its('0.contentDocument.body').then(cy.wrap).type('This is a test note')
-    cy.get('[data-qa="next"]').click()
+    cy.get('iframe').its('0.contentDocument.body').then(cy.wrap).type('This is a test note');
+    cy.get('[data-qa="next"]').click();
 
     // Finish Call
-    cy.get('[data-qa="finish-call"]').click()
+    cy.get('[data-qa="finish-call"]').click();
 
-  })
+  });
 
-})
+});
